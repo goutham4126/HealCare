003b4883802a4bc0ec5d04f94d89c71c6e000f75
goutham4126 <goutham4126@gmail.com>
2024-04-03 23:45:05 +0530

    committed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,32 +1,38 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
 import { HiHome } from "react-icons/hi2";
 import { FaVideo } from "react-icons/fa6";
 import { MdOutlineSchedule } from "react-icons/md";
 import { FaPrescriptionBottleMedical } from "react-icons/fa6";
-
+import { usePathname } from 'next/navigation';
 
 function Sidebar() {
+  const pathname=usePathname();
   const links=[
     {
         path:'/',
         name:'Home',
         icon:<HiHome className="h-6 w-6"/>,
+        isActive:pathname==="/"
     },
     {
         path:'https://200h323s3236cxkluds3455ukjxnj34467t4732-healcare-videochat.vercel.app/create',
         name:'Video',
         icon:<FaVideo className="h-6 w-6"/>,
+        isActive:pathname==="https://200h323s3236cxkluds3455ukjxnj34467t4732-healcare-videochat.vercel.app/create"
     },  
     {
         path:'/schedule',
         name:'Scheduler',
-        icon:<MdOutlineSchedule className="h-6 w-6"/>
+        icon:<MdOutlineSchedule className="h-6 w-6"/>,
+        isActive:pathname==="/schedule"
     },
     {
         path:'/prescription',
         name:'Prescription',
-        icon:<FaPrescriptionBottleMedical className="h-6 w-6"/>
+        icon:<FaPrescriptionBottleMedical className="h-6 w-6"/>,
+        isActive:pathname==="/prescription"
     }
   ]
   return (
@@ -35,7 +41,7 @@ function Sidebar() {
               {
                   links.map((link,index)=>
                   <div key={index}>
-                    <Link href={link.path} className="flex items-center gap-2 bg-teal-500 rounded-md text-white font-semibold"><span className="p-3">{link.icon}</span>
+                    <Link href={link.path} className={`${link.isActive?"text-teal-500 bg-white border-2 border-teal-500": "bg-teal-500 text-white"} flex items-center gap-2 rounded-md font-semibold`}><span className="p-3">{link.icon}</span>
                     <span>{link.name}</span></Link>
                   </div>)
               }
