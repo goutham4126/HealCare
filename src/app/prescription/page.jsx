9e6004b59eba9316ee437d27c6d527a912934895
goutham4126 <goutham4126@gmail.com>
2024-04-18 19:59:01 +0530

    bot

--- src/app/prescription/page.jsx
+++ src/app/prescription/page.jsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 import OpenAI from "openai";
 import { IoMdRefresh } from "react-icons/io";
 import { FaSearch } from "react-icons/fa";
+import { useSession } from "next-auth/react";
 
 export const openai = new OpenAI({ 
     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true,
 })
 export default function Chatbot() {
+    const {data:session}=useSession()
     const [userInput, setUserInput] = useState('');
     const [chatHistory, setChatHistory] = useState([]);
 
@@ -19,7 +21,7 @@ export default function Chatbot() {
         setUserInput('');
         try{
             const chatCompletion = await openai.chat.completions.create({
-                messages: [...chatHistory, { role: 'assistant', content: `I am suffering from ${userInput}.Give me food and exercises to overcome that disease.Give me point wise content.` }],
+                messages: [...chatHistory, { role: 'assistant', content: `I am suffering from ${userInput}.Give me food and exercises to overcome that disease in 5 sentences.` }],
                 model: 'gpt-3.5-turbo',
             });
 
@@ -38,9 +40,9 @@ export default function Chatbot() {
     };
 
     return (
-        <div className="w-[95%] sm:w-[70vw] lg:w-[35vw] m-auto bg-teal-400 rounded-xl" style={{marginTop:60,boxShadow:"rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px"}}>
+        <div className="w-[95%] sm:w-[70vw] lg:w-[35vw] m-auto bg-teal-400 rounded-xl" style={{marginTop:80,boxShadow:"rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px"}}>
             <div onClick={()=>setChatHistory([])}className="flex justify-between p-3">
-                <h1 className="font-bold">HealCare</h1>
+                <h1 className="font-semibold">HealCare</h1>
                 <h1><IoMdRefresh className="h-6 w-6"/></h1>
             </div>
             <div className="h-[60vh] overflow-auto p-2 bg-white">
@@ -49,20 +51,20 @@ export default function Chatbot() {
                         <div key={index} className="text-gray-500">
                             <div>
                                 {
-                                  message.role === 'user' ? <img src="https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/corporate-user-icon.png" alt="#" className="h-6 w-6 rounded-full"/> : 
-                                  <img src="https://t3.ftcdn.net/jpg/03/22/38/32/360_F_322383277_xcXz1I9vOFtdk7plhsRQyjODj08iNSwB.jpg" alt="#" className="h-8 w-8 rounded-full"/>
+                                  message.role === 'user' ? <img src={session.user.image} alt="#" className="h-7 w-7 rounded-full"/> : 
+                                  <img src="https://miro.medium.com/v2/resize:fit:962/1*I9KrlBSL9cZmpQU3T2nq-A.jpeg" alt="#" className="h-7 w-7 rounded-full border border-teal-400 p-1"/>
                                 }
                             </div>
-                            <div className={`${message.role === 'user' ?"p-2 mb-1 mt-1":"p-2 mb-1"} rounded-md font-extralight`}>
+                            <div className={`${message.role === 'user' ?"my-1.5 bg-slate-200 p-2":"p-2 bg-neutral-200 my-1.5"} rounded-md`}>
                                 {message.content}
                             </div>  
                         </div>
                     ))
                 }
             </div>
-            <div className="flex justify-center p-2 rounded-md">
-                    <input type="text" className="p-2 w-full outline-none text-gray-500" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
-                    <button onClick={handleUserInput} className="text-teal-400 bg-white p-2"><FaSearch className="h-6 w-5"/></button>
+            <div className="flex justify-center p-2">
+                    <input type="text" className="p-2 w-full outline-none rounded-s-md" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+                    <button onClick={handleUserInput} className="text-teal-400 bg-white p-2 rounded-e-md"><FaSearch className="h-6 w-5"/></button>
             </div>
         </div>
         
